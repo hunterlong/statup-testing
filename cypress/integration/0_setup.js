d5d7b0b6c7d74bfb99ff514d1685d5d100fc758f
Hunter Long <Info@socialeck.com>
2018-07-02 00:03:12 -0700

    travis testing

--- cypress/integration/0_setup.js
+++ cypress/integration/0_setup.js
@@ -5,7 +5,8 @@ context('Setup Process', () => {
         cy.get('input[name="db_host"]').clear().type(Cypress.env('DB_HOST'))
         cy.get('input[name="db_port"]').clear().type('5432')
         cy.get('input[name="db_user"]').clear().type(Cypress.env('DB_USER'))
-        cy.get('input[name="db_password"]').clear().type(Cypress.env('DB_PASS'))
+        cy.get('input[name="db_password"]').clear()
+        // cy.get('input[name="db_password"]').clear().type(Cypress.env('DB_PASS'))
         cy.get('input[name="db_database"]').clear().type(Cypress.env('DB_DATABASE'))
         cy.get('input[name="project"]').clear().type('Demo Tester')
         cy.get('input[name="description"]').clear().type('This is a test from Crypress!')
@@ -22,5 +23,5 @@ context('Setup Process', () => {
         cy.get('.service_li').should('have.length', 4)
         cy.get('.card').should('have.length', 4)
     })
-    
+
 });
